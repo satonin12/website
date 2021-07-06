fc6311630ad87246821b08cbb8bb23cf79251207
Vladislav <vlad.satonin74@gmail.com>
2021-07-06 15:03:48 +0500

    routing_added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+  Switch,
+  Route,
+  Redirect,
+  BrowserRouter as Router,
+} from 'react-router-dom'
 
 import './index.css'
 
-import ProductList from './component/ProductList.js'
+import Home from './pages/Home.js'
+import ProductList from './pages/ProductList.js'
+import TimersDashboard from './pages/TimersDashboard.js'
 
 ReactDOM.render(
   <>
-    <div className="wrapper">
-      <h4>Popular products</h4>
-    </div>
-    <hr size={1} width={500} />
-    <ProductList />
+    {/* TODO: add header <-> navbar */}
+    <Router>
+      <Switch>
+        <Route path="/home" exact>
+          <Home />
+        </Route>
+        <Route path="/product_list" exact>
+          <ProductList />
+        </Route>
+        <Route path="/timers" exact>
+          <TimersDashboard />
+        </Route>
+        <Redirect to="/home" />
+      </Switch>
+    </Router>
   </>,
   document.getElementById('root')
 )
