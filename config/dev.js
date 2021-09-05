06be25517698ed4724f934e485a2ad5c6ce70433
Vladislav <vlad.satonin74@gmail.com>
2021-09-05 19:24:57 +0500

    publish to vercel

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "jwtSecret": "vsatonin is Argyash",
+  "mongoUri": "mongodb+srv://vsatonin:vsatonin12@cluster0.w7alq.mongodb.net/app?retryWrites=true&w=majority"
+}
---
+++ config/production.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "jwtSecret": process.env.JWT_SECTET,
+  "mongoUri": process.env.MONGOURI
+}
